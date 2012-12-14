5b065e62eab09dbfa84d84d6cfd480c708f83381
Bjorn Stromberg <bstromberg@wizcorp.jp>
2012-12-14 18:36:43 +0900

    - Added version property, gets incremented on diff.  - renamed startBatch to pause  - renamed endBatch to resume  - renamed batch to consume  - renamed consume to merge  - made it so that add/del/destroy/rename events can also emit on resume instead of always emitting instantly  - created tests for pause/resume

---
+++ test/modules/pause.js
@@ -0,0 +1,122 @@
+var tomes = require('../../tomes');
+
+var Tome = tomes.Tome;
+
+exports.testPauseSetResume = function (test) {
+	test.expect(15);
+
+	var signalCount = 0;
+	var addCount = 0;
+
+	var a = { a: 1 };
+	var b = Tome.conjure(a);
+
+	b.on('signal', function (val) {
+		signalCount += 1;
+		test.strictEqual(JSON.stringify(a), JSON.stringify(val));
+	});
+
+	b.on('add', function (key, val) {
+		addCount += 1;
+		test.strictEqual(a[key], val);
+	});
+
+	a.b = 1;
+	a.c = 1;
+	a.d = 1;
+	a.e = 1;
+	a.f = 1;
+	a.g = 1;
+	a.h = 1;
+	a.i = 1;
+	a.j = 1;
+	a.k = 1;
+
+	b.pause();
+	b.set('b', 1).set('c', 1).set('d', 1).set('e', 1).set('f', 1).set('g', 1).set('h', 1).set('i', 1).set('j', 1).set('k', 1);
+	test.strictEqual(addCount, 0);
+	b.resume();
+
+	test.strictEqual(signalCount, 2);
+	test.strictEqual(addCount, 10);
+
+	test.done();
+};
+
+exports.testPauseDelResume = function (test) {
+	test.expect(15);
+
+	var signalCount = 0;
+	var delCount = 0;
+
+	var a = { a: 1, b: 1, c: 1, d: 1, e: 1, f: 1, g: 1, h: 1, i: 1, j: 1, k: 1 };
+	var b = Tome.conjure(a);
+
+	b.on('signal', function (val) {
+		signalCount += 1;
+		test.strictEqual(JSON.stringify(a), JSON.stringify(val));
+	});
+
+	b.on('del', function (key) {
+		delCount += 1;
+		test.strictEqual(a[key], undefined);
+	});
+
+	delete a.b;
+	delete a.c;
+	delete a.d;
+	delete a.e;
+	delete a.f;
+	delete a.g;
+	delete a.h;
+	delete a.i;
+	delete a.h;
+	delete a.j;
+	delete a.k;
+
+	b.pause();
+	b.del('b').del('c').del('d').del('e').del('f').del('g').del('h').del('i').del('j').del('k');
+	test.strictEqual(delCount, 0);
+	b.resume();
+
+	test.strictEqual(signalCount, 2);
+	test.strictEqual(delCount, 10);
+
+	test.done();
+};
+
+exports.testPauseDestroyResume = function (test) {
+	test.expect(5);
+
+	var signalCount = 0;
+	var destroyCount = 0;
+
+	var a = { a: 1, b: 1, c: 1, d: 1, e: 1, f: 1, g: 1, h: 1, i: 1, j: 1, k: 1 };
+	var b = Tome.conjure(a);
+
+	b.on('signal', function (val) {
+		signalCount += 1;
+		test.strictEqual(JSON.stringify(a), JSON.stringify(val));
+	});
+
+	b.b.on('destroy', function () {
+		destroyCount += 1;
+	});
+
+	b.c.on('destroy', function () {
+		destroyCount += 1;
+	});
+
+	delete a.b;
+	delete a.c;
+
+	b.pause();
+	b.del('b').del('c');
+	test.strictEqual(destroyCount, 0);
+	b.resume();
+
+	test.strictEqual(signalCount, 2);
+	test.strictEqual(destroyCount, 2);
+
+	test.done();
+};
