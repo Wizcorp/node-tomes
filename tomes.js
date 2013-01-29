cfb0fc57bfe2bccd3786898538b2e100d47ac723
Bjorn Stromberg <bstromberg@wizcorp.jp>
2013-01-29 16:41:26 +0900

    some cleanup, rename no longer uses to. pop, shift, reverse no longer use a val.

--- tomes.js
+++ tomes.js
@@ -135,14 +135,6 @@ function destroy(tome) {
 	tome.removeAllListeners();
 }
 
-function isEmpty(o) {
-	for (var k in o) {
-		k = k;
-		return false;
-	}
-	return true;
-}
-
 function reset(tome) {
 
 	// The reset method deletes all properties on a Tome and turns it back into
@@ -229,7 +221,10 @@ function diff(tome, op, val, chain, pair) {
 		chain = buildChain(tome);
 	}
 
-	var newOp = { chain: chain, op: op, val: val };
+	var newOp = { chain: chain, op: op };
+	if (val !== undefined) {
+		newOp.val = val;
+	}
 
 	root.__diff__.push(newOp);
 
@@ -667,7 +662,7 @@ Tome.prototype.move = function (key, newParent, onewKey) {
 
 	if (newParent === this) {
 		var rO = {};
-		rO[key] = { to: onewKey };
+		rO[key] = onewKey;
 		return this.rename(rO);
 	}
 
@@ -681,19 +676,36 @@ Tome.prototype.move = function (key, newParent, onewKey) {
 		return this;
 	}
 
+	// Make a copy of the chains before we move them. For use in the diff.
+
 	var newParentChain = buildChain(newParent);
 	var oldParentChain = buildChain(this);
 
-	if (onewKey.toString() === newKey.toString() && newParent instanceof ArrayTome) {
-		var a = 9; // this is to stop lint from complaining, could be refactored.
-		a = 8;
-	} else if (onewKey.toString() !== newKey.toString()) {
+	// convert (if not already the case) the newParent to an ObjectTome if:
+	// - the key is not numeric, or:
+	// - the key is numeric, but the parent is not an ArrayTome
+
+	// if the newKey that you're adding to newParent is numeric, allow it only
+	// if newParent is an ArrayTome, else (if needed) convert newParent into an
+	// ObjectTome.
+
+	var keyIsNumeric = (onewKey.toString() === newKey.toString());
+	var parentIsArray = (newParent instanceof ArrayTome);
+
+	if (!keyIsNumeric || (keyIsNumeric && !parentIsArray)) {
+		// use the string version of the newKey
+
 		newKey = onewKey;
+
+		// ensure newParent is an ObjectTome
+
 		if (!(newParent instanceof ObjectTome)) {
 			reset(newParent);
 			newParent.__proto__ = ObjectTome.prototype;
 		}
 	}
+
+	// add newKey to newParent
 
 	if (newParent instanceof ArrayTome) {
 		var arr = newParent._arr;
@@ -701,6 +713,9 @@ Tome.prototype.move = function (key, newParent, onewKey) {
 
 		arr[newKey] = this[key];
 		newParent[newKey] = arr[newKey];
+
+		// If the newKey is longer than the length of the array, fill to that
+		// point.
 
 		if (newKey >= len) {
 			for (var i = len; i < newKey - 1; i += 1) {
@@ -713,16 +728,25 @@ Tome.prototype.move = function (key, newParent, onewKey) {
 		newParent[newKey] = this[key];
 	}
 
+	// remove key from oldParent.
+
 	if (this instanceof ArrayTome) {
+
+		// if oldParent is a tome, create an undefinedTome in it's place.
+
 		this._arr[key] = Tome.conjure(undefined, this, key);
 		this[key] = this._arr[key];
 	} else {
 		delete this[key];
 	}
 
+	// fix up newKey to reflect it's new location.
+
 	newParent[newKey].__parent__ = newParent;
 	newParent[newKey].__key__ = newKey;
 	newParent[newKey].__root__ = newParent.__root__;
+
+	// now emit the addition and deletion of the keys.
 
 	emitDel(this, key);
 	emitAdd(newParent, newKey);
@@ -775,6 +799,7 @@ Tome.prototype.merge = function (diff) {
 	for (var i = 0, len = diffs.length; i < len; i += 1) {
 		var currentDiff = diffs[i];
 		var tome = resolveChain(this, currentDiff.chain);
+
 		var newParent;
 		var opVal = currentDiff.val;
 		switch (currentDiff.op) {
@@ -816,6 +841,8 @@ Tome.prototype.merge = function (diff) {
 		case 'unshift':
 			tome.unshift.apply(tome, opVal);
 			break;
+		default:
+			throw new Error('Tome.merge - Unsupported operation: ' + currentDiff.op);
 		}
 	}
 };
@@ -919,13 +946,13 @@ ArrayTome.isArrayTome = function (o) {
 
 ArrayTome.prototype.valueOf = function () {
 	if (!this.__hidden__) {
-		return this._arr ? this._arr : [];
+		return this._arr || [];
 	}
 };
 
 ArrayTome.prototype.toJSON = function () {
 	if (!this.__hidden__) {
-		return this._arr ? this._arr : [];
+		return this._arr || [];
 	}
 };
 
@@ -1037,7 +1064,7 @@ ArrayTome.prototype.shift = function () {
 		this.length = this._arr.length;
 		destroy(o);
 		emitDel(this, key);
-		diff(this, 'shift', 1);
+		diff(this, 'shift');
 	}
 
 	return out ? out.valueOf() : out;
@@ -1061,7 +1088,7 @@ ArrayTome.prototype.pop = function () {
 		}
 
 		emitDel(this, len);
-		diff(this, 'pop', 1);
+		diff(this, 'pop');
 	}
 
 	return out ? out.valueOf() : out;
@@ -1093,7 +1120,7 @@ ArrayTome.prototype.reverse = function () {
 		this._arr[i].__key__ = i;
 	}
 
-	diff(this, 'reverse', 1);
+	diff(this, 'reverse');
 
 	return this;
 };
@@ -1165,7 +1192,7 @@ ArrayTome.prototype.rename = function () {
 	case 'string':
 	case 'number':
 		rO = {};
-		rO[arguments[0]] = { to: arguments[1] };
+		rO[arguments[0]] = arguments[1];
 		break;
 	default:
 		throw new TypeError('ArrayTome.rename - invalid arguments.');
@@ -1176,7 +1203,7 @@ ArrayTome.prototype.rename = function () {
 	var temporary = {};
 
 	for (oldKey in rO) {
-		newKey = rO[oldKey].to;
+		newKey = rO[oldKey];
 		if (!this.hasOwnProperty(oldKey)) {
 			throw new ReferenceError('ObjectTome.rename - Key is not defined: ' + oldKey);
 		}
@@ -1218,6 +1245,7 @@ ArrayTome.prototype.rename = function () {
 ArrayTome.prototype.sort = function () {
 	this._arr.sort.apply(this._arr, arguments);
 
+	var hasChanges = false;
 	var rO = {};
 
 	for (var i = 0, len = this._arr.length; i < len; i += 1) {
@@ -1225,13 +1253,14 @@ ArrayTome.prototype.sort = function () {
 			var oldKey = this._arr[i].__key__;
 			this._arr[i].__key__ = i;
 			this[i] = this._arr[i];
-			rO[oldKey] = { to: i };
+			rO[oldKey] = i;
 			emitDel(this, oldKey);
 			emitAdd(this, i);
+			hasChanges = true;
 		}
 	}
 
-	if (!isEmpty(rO)) {
+	if (hasChanges) {
 		diff(this, 'rename', rO);
 	}
 
@@ -1562,7 +1591,7 @@ ObjectTome.prototype.rename = function () {
 		break;
 	case 'string':
 		rO = {};
-		rO[arguments[0]] = { to: arguments[1] };
+		rO[arguments[0]] = arguments[1];
 		break;
 	default:
 		throw new TypeError('ObjectTome.rename - Invalid arguments');
@@ -1572,7 +1601,7 @@ ObjectTome.prototype.rename = function () {
 	var temporary = {};
 
 	for (var oldKey in rO) {
-		var newKey = rO[oldKey].to;
+		var newKey = rO[oldKey];
 
 		if (!this.hasOwnProperty(oldKey)) {
 			throw new ReferenceError('ObjectTome.rename - Key is not defined: ' + oldKey);
