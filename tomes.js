8debe3ae0f33b2c37b81ad15039fec5c042b9e13
Bjorn Stromberg <bstromberg@wizcorp.jp>
2013-02-15 13:20:23 +0900

    Added getKey method.

--- tomes.js
+++ tomes.js
@@ -495,6 +495,10 @@ Tome.prototype.getVersion = function () {
 	return this.__root__.__version__;
 };
 
+Tome.prototype.getKey = function () {
+	return this.__key__;
+};
+
 Tome.prototype.set = function (key, val) {
 
 	// We use this to set a property on a Tome to the specified value. This can
