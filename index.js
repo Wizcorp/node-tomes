5e0699d44bb29414826360908789b548f5ec3f5a
Ron Korving <rkorving@wizcorp.jp>
2017-01-20 17:07:10 +0900

    Be nice to Node 0.10 and 0.12

--- index.js
+++ index.js
@@ -19,7 +19,7 @@
 // OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE
 // USE OR OTHER DEALINGS IN THE SOFTWARE.
 
-var EventEmitter = require('events');
+var EventEmitter = require('events').EventEmitter;
 
 function inherits(Child, Parent) {
 	Child.prototype = Object.create(Parent.prototype, {
